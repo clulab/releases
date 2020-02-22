37da6dd8f0a6a09d7db4c896640922765a64aa33
mithun <mithunpaul08@gmail.com>
2020-02-21 21:49:42 -0700

    had somehow missed uploading the prereq shell script

---
+++ emnlp2019-masking/prereq.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+pip install tqdm
+pip install clean-text
+pip install git+https://github.com/myedibleenso/py-processors.git
+mkdir -p outputs
+mkdir -p sstagged_files
+mkdir -p data
+
